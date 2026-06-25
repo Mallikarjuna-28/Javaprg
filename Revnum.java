c49114f478703502ac023f07f868d54d5afbc7df
siriharitha <siriharitha07@gmail.com>
2026-06-25 11:08:32 +0530

    Reverse of Number committed

---
+++ Revnum.java
@@ -0,0 +1,18 @@
+class Revnum {
+    public static void main(String args[]) {
+
+        int n = Integer.parseInt(args[0]);
+        int rev = 0;
+        int r;
+
+        while (n > 0) {
+            r = n % 10;
+            rev = (rev * 10) + r;
+            n = n / 10;
+        }
+
+        System.out.println("Reverse of Number is " + rev);
+    }
+}
+~                                                                                                                                                       
+~                     
